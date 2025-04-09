1bd95e3cb07068b308bbf3a8ccffd6fc3494e3dd
julian1111111 <145719754+julian1111111@users.noreply.github.com>
2025-04-09 10:09:49 -0400

    Updates data_processor.py to handle new data csv files

--- src/data_processor.py
+++ src/data_processor.py
@@ -14,33 +14,57 @@ class DataProcessor:
     def clean_data(self) -> pd.DataFrame:
         """Remove rows with missing values and normalize temperature data"""
         df = self.load_data().dropna()
-        min_maxTemp = df["temperature_2m_max"].min()
-        max_maxTemp = df["temperature_2m_max"].max()
-        df["temperature_2m_max_normalized"] = (df["temperature_2m_max"] - min_maxTemp) / (max_maxTemp - min_maxTemp)
-
-        min_minTemp = df["temperature_2m_min"].min()
-        max_minTemp = df["temperature_2m_min"].max()
-        df["temperature_2m_min_normalized"] = (df["temperature_2m_min"] - min_minTemp) / (max_minTemp - min_minTemp)
-
+        if "GlobalLandTemperaturesByCountry" in self.file_path:
+            df["AverageTemperatureNormalized"] = (df["AverageTemperature"] - df.groupby("Country")["AverageTemperature"].transform("min")) / (df.groupby("Country")["AverageTemperature"].transform("max") - df.groupby("Country")["AverageTemperature"].transform("min"))
+            # df["MinAvg"] = df.groupby("Country")["AverageTemperature"].transform("min")
+            # df["MaxAvg"] = df.groupby("Country")["AverageTemperature"].transform("max")
+        elif "GlobalLandTemperaturesByMajorCity" in self.file_path:
+            df["AverageTemperatureNormalized"] = (df["AverageTemperature"] - df.groupby(["Country", "City"])["AverageTemperature"].transform("min")) / (df.groupby(["Country", "City"])["AverageTemperature"].transform("max") - df.groupby(["Country", "City"])["AverageTemperature"].transform("min"))
+            # df["MinAvg"] = df.groupby(["Country", "City"])["AverageTemperature"].transform("min")
+            # df["MaxAvg"] = df.groupby(["Country", "City"])["AverageTemperature"].transform("max")
         return df
 
     def get_features_and_target(self) -> Tuple[np.ndarray, np.ndarray]:
         """Split data into features (month, day) and target (high, low)"""
         df = self.clean_data()
-        df["month"] = pd.to_numeric(df["time"].astype(str).str.split("-").str[1])
-        df["day"] = pd.to_numeric(df["time"].astype(str).str.split("-").str[2])
+        df["year"] = pd.to_numeric(df["dt"].astype(str).str.split("-").str[0])
+        df["month"] = pd.to_numeric(df["dt"].astype(str).str.split("-").str[1])
+        df["day"] = pd.to_numeric(df["dt"].astype(str).str.split("-").str[2])
 
-        features = df[["month", "day"]].to_numpy()
-        target = df[["temperature_2m_max", "temperature_2m_min"]].to_numpy()
+        if "GlobalLandTemperaturesByCountry" in self.file_path:
+            features = df[["Country", "year", "month", "day"]].to_numpy()
+        elif "GlobalLandTemperaturesByMajorCity" in self.file_path:
+            features = df[["City", "Country", "year", "month", "day"]].to_numpy()
+        target = df["AverageTemperature"].to_numpy()
 
         return features, target
 
 
 
 if __name__ == "__main__":
-    pass
-    # features, target = DataProcessor("../data/Temperature.csv").get_features_and_target()
+    country_df = DataProcessor("../data/GlobalLandTemperaturesByCountry.csv").clean_data()
+    # print(country_df.head())
+    # print(country_df.tail())
+    #
+    city_df = DataProcessor("../data/GlobalLandTemperaturesByMajorCity.csv").clean_data()
+    # print(city_df.head())
+    # print(city_df.tail())
+
+    features, target = DataProcessor("../data/GlobalLandTemperaturesByMajorCity.csv").get_features_and_target()
+    # count = 0
     # for i, j in zip(features, target):
-    #     if i[0] == 1:
-    #         print(f"{i}\t{j}")
+    #     count += 1
+    #     if count == 10:
+    #         break
+    #     print(f"{i}\t{j}")
+
+    features, target = DataProcessor("../data/GlobalLandTemperaturesByCountry.csv").get_features_and_target()
+    # count = 0
+    # for i, j in zip(features, target):
+    #     count += 1
+    #     if count == 10:
+    #         break
+    #     print(f"{i}\t{j}")
+
+
 
